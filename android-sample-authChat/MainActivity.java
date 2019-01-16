fcc782d1b2ab0ab6da8dace105c33fd81a293083
ARossWilliams <46757822+ARossWilliams@users.noreply.github.com>
2019-01-16 18:08:54 +0000

    Update MainActivity.java

--- android-sample-authChat/MainActivity.java
+++ android-sample-authChat/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements LiveAssistDelegat
     private void onCreateLiveAssist(){
 
         String[] sections = {""};
-        LiveAssistConfig liveAssistConfig = new LiveAssistConfig(19956324,sections,LiveAssistChatStyle.AUTO);
+        LiveAssistConfig liveAssistConfig = new LiveAssistConfig("PutAccountNumberHere",sections,LiveAssistChatStyle.AUTO);
 
         //{Auth Chat} Set the method name and LiveAssistDelegate on your LiveAssistConfig Object before you initialize the LiveAssistView
         liveAssistConfig.setJavascriptMethodName("authoriseChatWithCallback");
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements LiveAssistDelegat
     //{Auth Chat} Implement the authoriseChatWithCallback method and pass your JWT token to the authorise method.
     @Override
     public void authoriseChatWithCallback(LiveAssistAuth liveAssistAuth) {
-        String jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MDJDRTBGMS03OTBELUU3MTEtODBFOS1DNDM0NkJBQ0ZCQkMiLCJnaXZlbl9uYW1lIjoiT3B0aW9uYWwiLCJpc3MiOiJzb3VyY2Ugc3RyaW5nIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE3MTYyMzkwMjJ9.znR7SO2Ibz4ZQR07GbSMnvHBSFI4HZ4e2xS4AjG6JBllP5VWdMzg4YZJSAlo6-UV9HvljA3Oc3cIyqHa9SVqZg6PugQeRg5bwu1HwawvikXWL2hW-QS3l9JaUsY267ZL-FEp9402U0hsB96hOKsX-ppPM1cn7Ky1jRyJbeaYvMI";
+        String jwt = "PutJWTHere";
         liveAssistAuth.authorise(jwt);
     }
 }
